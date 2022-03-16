ed5e1d4f45d531c05348ccc66500fd1072af17d1
wojkos9 <wojkos9@gmail.com>
2022-03-16 09:47:40 +0100

    main loop

--- keccak.py
+++ keccak.py
@@ -1,4 +1,4 @@
-from utils import to_bits
+from utils import to_bits, xorv
 
 
 def pad_data(data: list[int], r: int) -> list[int]:
@@ -8,12 +8,26 @@ def pad_data(data: list[int], r: int) -> list[int]:
     return data + pad
 
 
-def sha3_256_enc(data: bytes):
-    r = 256
+def block_perm(block: list[int]) -> list[int]:
+    pass
+
+def sha3_256_enc(data: bytes) -> list[int]:
+    r = 1088
+    c = 512
     l = 6
+    d = 256
+    b = r + c
+    state = [0] * b
     bits = to_bits(data)
     padded = pad_data(bits, r)
-    print(padded)
+    n = len(padded)
+    for i in range(0, n, r):
+        block = padded[i:i+r]
+        tmp = block + [0] * c
+        tmp = xorv(tmp, state)
+        state = block_perm(tmp)
+    res = state[:d]
+    return res
 
 
 if __name__ == "__main__":
